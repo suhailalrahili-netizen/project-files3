84fe6d8ce7c7e5c182a77fda66f0786ab9918035
suhailalrahili-netizen <suhailalrahili@gmail.com>
2026-05-07 17:57:10 +0300

    Add files via upload

---
+++ 004.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct employee{
+    int id;
+    char name;
+    int age ;
+ 
+};
+int main(){
+    void *ptr = malloc(sizeof(struct employee));
+    struct employee *sptr = (struct employee*)ptr;
+    sptr->id = 0x44eeff11;
+    sptr->name ='S';
+    sptr->age = 0x663388dd;
+    FILE *fh = fopen ("employee.emp","wb");
+    if (fh != NULL){
+        fwrite(ptr,sizeof(struct employee), 1,fh);
+        fclose(fh);
+        printf("----------------------\n");
+    }
+
+    fh = fopen ("employee.emp","rb");
+     if (fh != NULL){
+        fread(ptr,sizeof(struct employee), 1,fh);
+        fclose(fh);
+        printf("id: %x\n", sptr->id);
+        printf("id: %c\n", sptr->name);
+        printf("id: %x\n", sptr->age);
+     }
+    
+    free(sptr);
+    return 0;
+
+}
